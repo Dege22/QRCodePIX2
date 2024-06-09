da48520acaac4fc63f0a7c7f4ead5d62802bd527
Dege22 <129922477+Dege22@users.noreply.github.com>
2024-06-09 20:31:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from pydantic import BaseModel
 from datetime import datetime
 import hashlib
@@ -80,43 +81,28 @@ def increment_url_counter(url):
 
 app = FastAPI()
 
-# Add CORS middleware
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:10000",
-    "http://localhost:33028",
-    "http://10.207.97.28:5173",
-    "http://10.207.97.28:4173",
-    # Add more origins if needed
-]
+# Forçar HTTPS
+app.add_middleware(HTTPSRedirectMiddleware)
 
+# Adicionar middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],  # Permitir todas as origens
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Serve Static Files, used for QR code image access
-print("Diretório atual:", os.getcwd())
+# Servir arquivos estáticos
 if not os.path.exists('static'):
     os.makedirs('static')
-    print("Diretório 'static' criado em:", os.path.abspath('static'))
-else:
-    print("Diretório 'static' já existe em:", os.path.abspath('static'))
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 def generate_txid() -> str:
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     hash_digest = hashlib.sha1(timestamp.encode()).hexdigest()[:10]
     txid = f"{timestamp}{hash_digest}"
     return txid
-
 
 @app.get("/generate-pix/{chave_aleatoria}/{nome_beneficiario}/{cidade_beneficiario}/{valor_transferencia:float}")
 async def generate_pix(request: Request, chave_aleatoria: str, nome_beneficiario: str, cidade_beneficiario: str,
@@ -162,7 +148,6 @@ async def generate_pix(request: Request, chave_aleatoria: str, nome_beneficiario
             "Console Output": new_stdout.getvalue().strip()
         })
 
-
 @app.get("/download/{filename}")
 async def download_qr_code(filename: str):
     file_path = f"static/{filename}"
@@ -170,12 +155,10 @@ async def download_qr_code(filename: str):
         return FileResponse(path=file_path, filename=filename, media_type='image/png')
     raise HTTPException(status_code=404, detail="File not found")
 
-
 @app.get("/pix/gerados")
 async def get_total_pix_generated():
     total_pix_generated = read_counter()
     return JSONResponse(content={"Total de pix gerados": total_pix_generated})
-
 
 @app.get("/generate-pix-fixo/{valor_transferencia:float}")
 async def generate_pix_fixo(request: Request, valor_transferencia: float):
@@ -228,12 +211,10 @@ async def generate_pix_fixo(request: Request, valor_transferencia: float):
             "Console Output": new_stdout.getvalue().strip()
         })
 
-
 @app.get("/trocar-chave/{chave_aleatoria}")
 async def trocar_chave(chave_aleatoria: str):
     write_fixed_key(chave_aleatoria)
     return JSONResponse(content={"message": f"Chave aleatória substituída com sucesso! Nova chave: {chave_aleatoria}"})
-
 
 @app.get("/consultar-dados-fixos")
 async def consultar_dados_fixos():
@@ -251,18 +232,15 @@ async def consultar_dados_fixos():
         "Cidade": cidade_beneficiario
     })
 
-
 @app.get("/zerar-contagem-pix")
 async def zerar_contagem_pix():
     reset_pix_counter()
     return JSONResponse(content={"message": "Contagem de Pix gerados zerada com sucesso!"})
 
-
 @app.get("/zerar-contagem-visitantes")
 async def zerar_contagem_visitantes():
     reset_url_counter()
     return JSONResponse(content={"message": "Contagem de acessos zerada com sucesso!"})
-
 
 @app.get("/contar-acesso/{var:path}")
 async def contar_acesso_var(request: Request, var: str):
@@ -270,7 +248,6 @@ async def contar_acesso_var(request: Request, var: str):
     url = f"{base_url}/contar-acesso/{var}"
     increment_url_counter(url)
     return JSONResponse(content={"message": f"Acesso registrado para URL: {url}"})
-
 
 @app.get("/acessos")
 async def get_acessos():
